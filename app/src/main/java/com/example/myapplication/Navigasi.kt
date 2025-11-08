eb3d37236053e4ddbacea72f0ae60688227d5312
dianfitrip <dianfitri683@gmail.com>
2025-11-08 22:19:11 +0700

    nambahin import

--- app/src/main/java/com/example/myapplication/Navigasi.kt
+++ app/src/main/java/com/example/myapplication/Navigasi.kt
@@ -1,5 +1,17 @@
 package com.example.myapplication
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.myapplication.view.FormIsian
+import com.example.myapplication.view.HalamanHome
+import com.example.myapplication.view.ListPesertaScreen
 
 enum class Navigasi {
     Home,
