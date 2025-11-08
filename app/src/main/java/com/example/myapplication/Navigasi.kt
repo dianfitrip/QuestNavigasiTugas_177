46acac5cc2312a156edde79dd650a2da9b62da27
dianfitrip <dianfitri683@gmail.com>
2025-11-08 22:16:06 +0700

    menambahkan NavHost untuk navigasi aplikasi dengan halaman Home sebagai halaman awal

--- app/src/main/java/com/example/myapplication/Navigasi.kt
+++ app/src/main/java/com/example/myapplication/Navigasi.kt
@@ -6,3 +6,23 @@ enum class Navigasi {
     Formulirku,
     ListPeserta
 }
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController()
+){
+    Scaffold(modifier = Modifier.fillMaxSize()) { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Home.name,
+            modifier = Modifier.padding(paddingValues = isiRuang)
+        ) {
+            // 1. Halaman Home
+            composable(route = Navigasi.Home.name) {
+                HalamanHome(
+                    onMasukClick = { navController.navigate(Navigasi.ListPeserta.name) }
+                )
+            }
+        }
+    }
+}
