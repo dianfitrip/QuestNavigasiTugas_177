7fe19945e6db6ba8b3474ef9dc61f25b30ca959c
dianfitrip <dianfitri683@gmail.com>
2025-11-08 21:29:20 +0700

    menambahkan teks Selamat Datang dengan font semi-bold dan warna merah serta Spacer di bawahnya

--- app/src/main/java/com/example/myapplication/view/HomePage.kt
+++ app/src/main/java/com/example/myapplication/view/HomePage.kt
@@ -25,7 +25,15 @@ fun HalamanHome(
                     .padding(vertical = dimensionResource(id = R.dimen.padding_xlarge)),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.SpaceBetween
-            )
+            ){
+                Text(
+                    text = stringResource(id = R.string.selamat_datang),
+                    fontSize = dimensionResource(id = R.dimen.font_size_header).value.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = colorResource(id = R.color.red)
+                )
+                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.spacer_xlarge)))
+            }
         }
     }
 }
